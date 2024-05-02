52f90c6fc04de3236240e2234fcb4773996d6800
ix520 <love.liu.520@qq.com>
2024-05-02 18:29:35 +0800

    Site updated: 2024-05-02 18:29:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/01/你好/"];function toRandomPost(){
+var posts=["2023/05/01/你好/","2024/05/02/about/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
