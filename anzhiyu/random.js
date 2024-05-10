3154842f045b0594b4c7fae83876d43009cb06e4
ix520 <love.liu.520@qq.com>
2024-05-10 23:58:39 +0800

    Site updated: 2024-05-10 23:58:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/01/你好/"];function toRandomPost(){
+var posts=["2023/05/01/你好/","2024/05/10/移动云手机搭建网站或alist/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
